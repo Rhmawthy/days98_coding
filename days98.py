f901e35f7b4a5562250ffcdf3066872431af5017
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2023-01-15 21:23:14 +0800

    Add files via upload

---
+++ days98.py
@@ -0,0 +1,7 @@
+''' Arif mendorong meja sehingga berpindah 6m jika gaya dorong yang di keluarkan Arif sebesar 10N, berapa usaha yang terjadi akibat peristiwa itu '''
+
+F = 10
+s = 6 
+
+w = 10 * 6 
+print("usaha yang terjadi akibat peristiwa, sebesar", w,"joule")
